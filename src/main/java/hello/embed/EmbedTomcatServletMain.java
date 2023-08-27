e1661f2c4ab7571f445a70ffe4fcd755dd8ce7df
charles <wkc1002@naver.com>
2023-08-27 17:08:11 +0900

    Embed Tomcat - Spring

--- src/main/java/hello/embed/EmbedTomcatServletMain.java
+++ src/main/java/hello/embed/EmbedTomcatServletMain.java
@@ -10,6 +10,7 @@ public class EmbedTomcatServletMain {
 
     public static void main(String[] args) throws LifecycleException {
         System.out.println("EmbedTomcatServletMain.main");
+
         // 톰캣 설정
         Tomcat tomcat = new Tomcat();
         Connector connector = new Connector();
